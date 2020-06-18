e847ea1c829c26e9e1f502d0d0a2e83d2e7164ad
MitchellRB <43767850+MitchellRB@users.noreply.github.com>
2020-06-18 16:36:29 +1000

    Basic node functions

--- BinaryTree/TreeNode.h
+++ BinaryTree/TreeNode.h
@@ -5,9 +5,20 @@ public:
 	TreeNode(int _value);
 	~TreeNode();
 
+	bool hasLeft() { return (m_left != nullptr); };
+	bool hasRight() { return (m_right != nullptr); };
+
+	int getData() { return m_value; };
+	TreeNode* getLeft() { return m_left; };
+	TreeNode* getRight() { return m_right; };
+
+	void setData(int value) { m_value = value; };
+	void setLeft(TreeNode* node) { m_left = node; };
+	void setRight(TreeNode* node) { m_right = node; };
+
 protected:
-	int value;
-	TreeNode* leftChild;
-	TreeNode* rightChild;
+	int m_value;
+	TreeNode* m_left;
+	TreeNode* m_right;
 };
 
